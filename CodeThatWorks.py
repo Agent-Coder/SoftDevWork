15aa3c64a681ddcc33690696e559b403f842d79a
Amanda Zheng <azheng00@stuy.edu>
2019-09-10 08:41:51 -0400

    codethatworks

--- CodeThatWorks.py
+++ CodeThatWorks.py
@@ -1,1 +1,15 @@
-print "Code that Works"
+import random
+
+def randStudent(lis):
+    firstRand=random.randint(0,2)
+    if firstRand==0:
+        firstRand='orpheus'
+    elif firstRand==1:
+        firstRand='rex'
+    else:
+        firstRand='endymion'
+    print (lis.get(firstRand)[random.randint(0,len(lis.get(firstRand))-1)])
+Krewes={'orpheus':['aksjfoad','sadjofija','oisdfhahio'], 'rex':['ejsdfho','cidfh','biudhf'], 'endymion':['iodfu','laks','maios']}
+randStudent(Krewes)
+
+        
