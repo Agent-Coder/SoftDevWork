5a38d35c7e02c39ac055b32f672903fc7870a4a4
Agent-Coder <43050217+Agent-Coder@users.noreply.github.com>
2019-09-17 10:10:35 -0400

    started commenting

--- 03_csv/jobPickerM&M.py
+++ 03_csv/jobPickerM&M.py
@@ -1,22 +1,32 @@
 import random
 file=open("occupations.csv","r")
 job=file.read().split("\n")
-
+#opening, reading, and splitting the occupations thing on new lines
 def splitCategory(l):
+    #splits occupations and percentages
     for x in range(0,len(l)):
+        #loop through list 
         l[x]=l[x].rsplit(",",1)
+        #split at the last comma of each item (becomes 2d array with occupation and percentage)
     return l
 job=splitCategory(job)
 
 def takeOutQuotes(l):
+    #function for taking out quotes
     for x in range(0,len(l)):
+        #loop through the list
         for i in range(0,len(l[x])-1):
+            #loop through the occupation and then the percentage
             l[x][i]=l[x][i].strip("\"")
+            #strip the quotes
     return l
 job=takeOutQuotes(job)
 job=job[1:len(job)-2]
+#take out the first item(category titles) and last thing (total percentage) 
 def rangePercent(l):
+    #make 
     l[0][1]=float(l[0][1])
+    #make the first string percentage a float
     for x in range(1,len(l)):
         l[x][1]=round(float(l[x][1])+l[x-1][1],1)
     return l
