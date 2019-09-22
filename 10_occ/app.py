4a1f664996cc98583148465b04c3b832ca7f2c86
Amanda Zheng <azheng00@stuy.edu>
2019-09-22 12:40:08 -0400

    finished css and root routing

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,15 +1,33 @@
+# Amanda Zheng and Yaru Luo (AI-YA)
+# SoftDev1 pd1
+# K#10: Jinja Tuning
+# 2019-09-24
+
+from flask import Flask, render_template
+app = Flask(__name__) # create instance of class Flask
 import csv, random
+
 rows = []
 with open('occupations.csv', 'r') as csvFile:
     csvreader = csv.reader(csvFile)
     for row in csvreader:
         rows.append(row)
-
 dictionary = {}
 for item in rows[1: len( rows) - 1]:
     key = item[0]
     value = float( item[1])
     dictionary[ key] = value
+r = []
+with open('link.csv', 'r') as cFile:
+    csvread = csv.reader(cFile)
+    for row in csvread:
+        r.append(row)
+encyclopedia={}
+for item in r[0: len( r)]:
+    key = item[0]
+    value = item[1]
+    encyclopedia[ key] = value
+
 #make into a dictionary
 def rangePercent(l):
     di=list(l.keys())
@@ -21,7 +39,7 @@ def rangePercent(l):
         #if our random number is 5.3, since its less than the 6.1, the first item is returned
         #if 11.4 is generated, second item is returned because it is greater than 6.1 but less than 16.3
     return l
-rangePercent(dictionary)
+dictionary=rangePercent(dictionary)
 def randomPick(l):
     #generates a random percentage between 0 and 100%
     rand=random.randint(1,1000)
@@ -49,5 +67,25 @@ def randomPick(l):
     #front loop returns whatever item it counted up to
     else:
         return di[countb+1]
-    #back loop returns the next item 
-print (randomPick(dictionary))
+    #back loop returns the next item
+coll=dictionary
+col=encyclopedia
+@app.route("/") # assign following fxn to run when route requested
+def hello_world():
+    print(__name__+ "Norm")
+    return "Werk werk werk werk werk" #default localhost page
+
+@app.route("/occupyflaskst")
+def init():
+    print("csving")
+    return render_template(
+        'work.html',
+        coll = dictionary,   #for the tablified occupations list
+        output = randomPick(coll), #for the lucky occupation
+        col=encyclopedia
+        )
+
+if __name__ == "__main__":
+    #autopilot
+    app.debug = True
+    app.run()
