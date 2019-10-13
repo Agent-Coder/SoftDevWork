1d6d8cd866b12d9ea6105c84daaefccf9414b63b
Amanda Zheng <azheng00@stuy.edu>
2019-10-13 15:21:12 -0400

    finally finished at one hour 26 minutes 40 seconds

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -1,7 +1,7 @@
-# Ayham Alnasser && Mandy Zheng(?)
-# SoftDev
-# skeleton :: SQLITE3 BASICS
-# Oct 2019
+# Ayham Alnasser && Mandy Zheng (DELETING DB FILES)
+# SoftDev1 pd1
+# K#18 - Average
+# 2019-10-13
 
 import sqlite3   # enable control of an sqlite database
 import csv       # facilitate CSV I/O
@@ -14,11 +14,22 @@ c = db.cursor()               # facilitate db ops
 
 # ==========================================================
 
+while True:  # add as many students as the user wants
+    user_input = input("Add a grade? Y/N ")
+    if 'y' in user_input.lower():
+        user_input = input("Enter the students' course, mark, and id - separated by commas:\n")
+        if len(user_input.split(',')) == 3:
+            with open("courses.csv", 'a') as o_file:
+                o_file.write(user_input + '\n') # new line to move cursor to the next line in file
+        else:
+            print("Incorrect input; expected 3 arguments")
+    else:
+        break
 
-##POPULATE THE DB IF EMPTY OTHERWISE IGNORE
+# ==========================================================
 
 with open('students.csv') as students:
-    reader = csv.DictReader(students)
+    reader = csv.DictReader(students) # make a dictionary out of every header in csv
     command = 'CREATE TABLE students ' \
               '( name TEXT, ' \
               'age INTEGER, ' \
@@ -26,17 +37,15 @@ with open('students.csv') as students:
               ');'
     c.execute(command)
     for row in reader:
-        curr_name = row['name']
-        curr_age = str(row['age'])
-        curr_id = str(row['id'])
+        curr_name, curr_age, curr_id = row['name'], str(row['age']), str(row['id'])
         command = 'INSERT INTO students (name,age,id)' + '\n' + \
                   'VALUES ((' + '\'' + curr_name + '\'' '), ' \
                          '(' + curr_age + '), ' \
-                         '(' + curr_id + '));'
+                         '(' + curr_id + '));'  # add to sql table
         c.execute(command)
 
 with open('courses.csv') as courses:
-    reader = csv.DictReader(courses)
+    reader = csv.DictReader(courses) # same as both
     command = 'CREATE TABLE courses ' \
               '(code TEXT, ' \
               'mark INTEGER, ' \
@@ -54,41 +63,27 @@ with open('courses.csv') as courses:
         c.execute(command)
 
 # ==========================================================
+
 command = 'CREATE TABLE stu_avg' \
           '( id INTEGER, ' \
           'avg INTEGER ' \
           ');'
-c.execute(command)
+c.execute(command)  # create stu_avg table
 
-
-# ==========================================================
 command = "SELECT name,mark,students.id FROM courses, students WHERE courses.id=students.id;"
 c.execute(command)
-rows = c.fetchall()
-print(rows[0][1])
-name = rows[0][0]
-stud_id = rows[0][2]
-avg, name_count = 0, 0
+rows = c.fetchall()  # create a 2-d array of the outputs
+stud_dict = dict()
 for row in rows:
-    if row[0] == name:
-        avg += row[1]
-        name_count += 1
+    if row[2] in stud_dict.keys():  # catch any repeats if csv is not ordered bc of new inputs
+        stud_dict[row[2]].append(row[1])  # append new course grade into preexisting list
     else:
-        avg /= name_count
-        print(name, stud_id, avg)
-        command = 'INSERT INTO stu_avg (id, avg) VALUES ((' + str(stud_id) + '),' + '(' + str(avg) + '));'
-        c.execute(command)
-        name = row[0]
-        avg = row[1]
-        stud_id = row[2]
-        name_count = 1
+        stud_dict[row[2]] = [row[1]]  # make a list of the first element
 
-avg /= name_count
-print(name, stud_id, avg)
-command = 'INSERT INTO stu_avg (id, avg) VALUES ((' + str(stud_id) + '),' + '(' + str(avg) + '));'
-c.execute(command)
-
-
+for key in stud_dict.keys():
+    avg = sum(stud_dict[key]) / len(stud_dict[key])  # finds the average
+    command = 'INSERT INTO stu_avg (id, avg) VALUES ((' + str(key) + '),' + '(' + str(avg) + '));'
+    c.execute(command)
 
 # ==========================================================
 
