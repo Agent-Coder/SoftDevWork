23dbb6c021466f7c3a79514f4fc3ff490d59f869
azheng00 <azheng00@stuy.edu>
2020-03-15 22:10:37 -0400

    made file

---
+++ 11_mongoflask/anime.py
@@ -0,0 +1,27 @@
+#Amanda Zheng, Tiffany Cao, Team Blank
+#K10 -- Import/Export Bank
+#K11 --
+#2020-03-05
+from flask import Flask, render_template, request,session
+from pymongo import MongoClient
+import json
+
+client = MongoClient("localhost", 27017)
+anime = client.weeb.anime
+anime.drop()
+file = open("anime-offline-database.json", "r")
+doc = json.readlines()
+for x in doc:
+    anime.insert_one(loads(x))
+
+def findAnime(a):
+    return food.find( { "status": a },{ "name": 1,"_id":0} )
+
+for result in findAnime("FINISHED"):
+   print(result["title"])
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
