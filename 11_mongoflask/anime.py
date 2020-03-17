aba4381df77d4336cf15d9e37091e636f819115a
azheng00 <azheng00@stuy.edu>
2020-03-16 21:19:55 -0400

    finished

--- 11_mongoflask/anime.py
+++ 11_mongoflask/anime.py
@@ -1,55 +1,87 @@
 #Amanda Zheng, Tiffany Cao, Team Blank
-#K10 -- Import/Export Bank
 #K11 --
 #2020-03-05
+
 from flask import Flask, render_template, request,session
 from pymongo import MongoClient
 import json
 from bson.json_util import loads
+
+app = Flask(__name__)
 client = MongoClient("localhost", 27017)
 anime = client.weeb.anime
-anime.drop()
-file = open("anime.json", "r")
-doc = file.readlines()
-for x in doc:
-    anime.insert_one(loads(x))
+
+def create():
+    anime.drop()
+    file = open("anime.json", "r")
+    doc = file.readlines()
+    for x in doc:
+        anime.insert_one(loads(x))
+
+#create()
 
 def findStatus(stat):
-    return anime.find( { "status": stat })
+    if stat==0:
+        return anime.find({})
+    return anime.find({ "status": stat })
 
 def findTitle(name):
+    if name=="":
+        return anime.find({})
     name=".*"+name+".*"
-    return anime.find({"title": {"$regex":name,"$options":"i"}})
-def findEp(num):
-    return anime.find( { "episodes": {"$lte":num}})
+    #print(name)
+    #print(anime.find({ "title": { "$regex": name, "$options": "i" }}).count())
+    return anime.find({ "title": { "$regex": name, "$options" : "i" }})
+
+def findEp(num,mode):
+    if mode==0:
+        return anime.find({})
+    if(mode=="Less"):
+        return anime.find({ "episodes": { "$lte": num }})
+    else:
+        return anime.find({ "episodes": { "$gte": num }})
+
 def findType(type):
-    return anime.find({"type":type})
+    if type==0:
+        return anime.find({})
+    #print(type)
+    #print(anime.find({ "type": type }).count())
+    return anime.find({ "type": type })
 
-for result in findStatus("CURRENTLY"):
-    if (result["title"]==""):
-        print("No Name Found")
-    else:
-      print (result["title"])
+def findRand(num):
+    return anime.aggregate([{ "$sample": { "size": num }}])
 
-for result in findTitle("girl"):
-    if (result["title"]==""):
-        print("No Name Found")
-    else:
-      print (result["title"])
 
-for result in findEp(20):
-    if (result["title"]==""):
-        print("No Name Found")
-    else:
-      print (result["title"])
+#for result in findStatus("CURRENTLY"):
+    #if (result["title"]==""):
+      #print("No Name Found")
+    #else:
+      #print (result["title"])
 
-for result in findType("OVA"):
-    if (result["title"]==""):
-        print("No Name Found")
-    else:
-      print (result["title"])
-app = Flask(__name__)
+#for result in findTitle("girl"):
+    #if (result["title"]==""):
+      #print("No Name Found")
+    #else:
+      #print (result["title"])
+
+#for result in findEp(20):
+    #if (result["title"]==""):
+      #print("No Name Found")
+    #else:
+      #print (result["title"])
+
+#for result in findType("OVA"):
+    #if (result["title"]==""):
+      #print("No Name Found")
+    #else:
+      #print (result["title"])
+
+#for result in findRand(10):
+   #if (result["title"]==""):
+     #print("No Name Found")
+   #else:
+     #print (result["title"])
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    #app.run()
--- 11_mongoflask/app.py
+++ 11_mongoflask/app.py
@@ -1,52 +1,99 @@
 #Amanda Zheng, Tiffany Cao, Team Blank
-#K10 -- Import/Export Bank
-#K11 --
+#K11 -- Ay Mon Go Git It From Yer Flask
 #2020-03-05
-from flask import Flask, render_template, request,session
-from pymongo import MongoClient
-import json
-
-client = MongoClient("localhost", 27017)
-anime = client.weeb.anime
-anime.drop()
-file = open("anime-offline-database.json", "r")
-doc = json.load(file)
+import os
+from flask import Flask, render_template, request, session, redirect, flash, url_for
+import anime
 
 app = Flask(__name__)
+app.secret_key = "help"
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def form():
+    tags = False
+    if request.form.get('search') is not None:
+        tags = True
+    return render_template("home.html", tags = tags)
 
-    render_template("form.html")
 
+@app.route("/results", methods=['GET', 'POST'])
+def start():
+   # print(request.form.get('supporttype'))
+   # if request.form.get('supporttype') is not None:
+   #   flash("Here " + request.form.get('supporttype'))
+   #   return redirect(url_for('error'))
+    if request.form.get('random') is not None or request.form.get('refresh') is not None:
+      random = anime.findRand(10)
+      return render_template("results.html", query = False, rand = True, random = random)
+    # print("1" + request.args.get('findstuff'))
+    # print(request.args.get('type'))
+    # print(request.args.get('status'))
+    # print(request.args.get('episodes'))
+    # print("5" + request.args.get('eps'))
+    if (request.args.get('findstuff') == "" and
+       request.args.get('type') == "0" and
+       request.args.get('status') == "0" and
+       request.args.get('episodes') == "0"):
+       flash("Please enter search queries.")
+       return redirect(url_for('form'))
+    search = "None"
+    type = "None"
+    status = "None"
+    episodes = "None"
+    if request.args.get('findstuff'): search = request.args.get('findstuff')
+    if request.args.get('type') and request.args.get('type') != "0": type = request.args.get('type')
+    if request.args.get('status') and request.args.get('status') != "0": status = request.args.get('status')
+    if request.args.get('episodes') and request.args.get('episodes') != "0":
+        episodes = request.args.get('episodes')
+        if request.args.get('eps') == "":
+            flash("Please enter a number for episodes.")
+            return redirect(url_for('form'))
+        elif int(request.args.get('eps')) < 0:
+            flash("Please enter a valid episode number.")
+            return redirect(url_for('form'))
+        else:
+            episodes += " " + request.args.get('eps') + " episode(s)"
+    type=request.form["type"]
+    status=request.form["status"]
+    mode=request.form["episodes"]
+    ep=request.form["eps"]
+    title=request.form["findstuff"]
+    t=anime.findType(type)
+    s=anime.findStatus(status)
+    e=anime.findEp(ep,mode)
+    h=anime.findTitle(title)
+    loop=[]
+    if(len(s)<len(t) and len(s)<len(e) and len(s)<len(h)):
+        loop=s
+    elif(len(t)<len(s) and len(t)<len(e) and len(t)<len(h)):
+        loop=t
+    elif(len(e)<len(s) and len(e)<len(t) and len(e)<len(h)):
+        loop=e
+    else:
+        loop=h
+    results=[]
+    count =0
+    for x in loop:
+        count+=1
+        if (x in s) and (x in t) and (x in e) and (x in h):
+            results.append(x)
+        if count>50:
+            break
+    return render_template("results.html", query = True, rand = False, search = search, type = type, status = status, episodes = episodes)
 
-@app.route("/results", methods=['GET'])
-def finished():
-    li=[]
-    for x in anime.find({"staus": "FINISHED"}):
-        li.append(x["title"])
-    render_template("results.html", collection=li)
-def rec():
-    all=[]
-    li=[]
-    for x in anime.find({{},{"title":1} }):
-        all.append(x["title"]) #easier way??
-    for x in range(10):
-        n=random.randint(0,len(all)-1)
-        if n not in num:
-        num.append(n)
-        li.append(all[n])
-    render_template("results.html",collection=li)
-def gallery():
-    all=[]
-    pic=[]
-    li=[]
-    for x in anime.find({{},{"title":1} }):
-        all.append(x["title"]) #easier way??
-        pic.append(x["picture"])
-    render_template("gallery.html",titles=all,pic=pic)
+@app.route("/single", methods = ['GET', 'POST'])
+def one():
+    print(request.form.get('supporttype'))
+    if request.form.get('supporttype') is not None:
+      flash("Here " + request.form.get('supporttype'))
+      return redirect(url_for('error'))
+    return redirect(url_for('form'))
 
+@app.route("/error", methods=['GET'])
+def error():
+    return render_template("error.html")
 
 if __name__ == "__main__":
     app.debug = True
+    anime.create()
     app.run()
